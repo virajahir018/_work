fb07ba1d9f194d61403110b496e474cf133943f3
virajahir018 <virajahir018@gmail.com>
2025-11-20 14:26:20 +0530

    u

---
+++ Q5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, i;
+
+    printf("Enter Array size : ");
+    scanf("%d", &n);
+
+    int a[n], b[n], c[n];
+
+    printf("\nEnter Array A : \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("A [%d]", i);
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nEnter Array B : \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("B [%d]", i);
+        scanf("%d", &b[i]);
+    }
+
+    printf("\nArray C is : ");
+    for (i = 0; i < n; i++)
+    {
+        c[i] = a[i] + b[i];
+        printf("%d, ", c[i]);
+    }
+
+     printf("\nArray C is : ");
+    for (i = 0; i < n; i++)
+    {
+        c[i] = a[i] + b[i];
+        printf("%d, ", c[i]);
+    }
+}
